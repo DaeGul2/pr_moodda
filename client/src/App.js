d7c2ff546a4ac4656bbec98e1bdd7ac5d3d0f991
DaeGul2 <alsxogml1234@gmail.com>
2023-09-24 05:14:30 +0900

    Update App.js
    d

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
 import Dashboard from './pages/DashBoard/DashBoard';
 import SignUp from './pages/SignUp'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useNavigate
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-      {<SignUp></SignUp>}
-      {<Dashboard></Dashboard>}
-    </div>
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<Dashboard ></Dashboard>}></Route>
+        <Route path="/signup" element={<SignUp ></SignUp>}></Route>
+      </Routes>
+    </Router>
   );
 }
 
